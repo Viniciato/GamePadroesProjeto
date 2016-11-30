674ab6f0017605b79b064a726376602a3a4ed543
Vinicius N <vininadin@gmail.com>
2016-11-29 22:23:46 -0200

    Added Class Player

---
+++ src/Player.java
@@ -0,0 +1,18 @@
+
+public class Player {
+	private String name;
+	private String type;
+	
+	public Player(String name, String type){
+		this.name=name;
+		this.type=type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getType() {
+		return type;
+	}
+}
